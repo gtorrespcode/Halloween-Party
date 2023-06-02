dad7bb1cec1ea8ed73722cf7e0ed8a034111b973
João Vitor <devjohncb@gmail.com>
2023-06-02 20:01:45 -0300

    Started the InfoModal

--- src/contexts/NavContext.jsx
+++ src/contexts/NavContext.jsx
@@ -4,6 +4,7 @@ export const NavContext = createContext();
 
 export const NavProvider = ({ children }) => {
   const [navOpen, setNavOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
 
   const changeNav = () => {
     if (window.innerWidth < 903) {
@@ -11,8 +12,13 @@ export const NavProvider = ({ children }) => {
     }
   };
 
+  const openModal = ({target}) => {
+    setModalOpen(!modalOpen);    
+    console.log(target.innerText);
+  }
+
   return (
-    <NavContext.Provider value={{ navOpen, changeNav }}>
+    <NavContext.Provider value={{ navOpen, changeNav, openModal }}>
       {children}
     </NavContext.Provider>
   );
